6e670a7731fdacbd34f7acf4f17f5c52084be4b2
cboscarino <31283902+cboscarino@users.noreply.github.com>
2017-08-23 20:49:34 -0400

    Update fibonacci.php
    Removed HTML elements
    output formatted to json

--- fibonacci.php
+++ fibonacci.php
@@ -1,22 +1,5 @@
-<html>
-<head>
-<title>FibService</title>
-</head>
-  <body>
-
-<!-- Uncomment Below html if input box is desired -->
-
-<!--
-    <FORM NAME ="form" METHOD ="GET" ACTION = "index.php">
-      <INPUT TYPE = "TEXT" NAME = "integer" VALUE = "">
-      <INPUT TYPE = "Submit">
-    </FORM>
--->
-
-    <?PHP
-      $result = shell_exec('C:\cygwin64\bin\bash C:\cygwin64\home\Boscarino\scripts\fib.sh ' . $_GET['integer']);
-      print ($result);
-    ?>
-
-  </body>
-</html>
+<?PHP
+	$result = shell_exec('C:\cygwin64\bin\bash C:\cygwin64\home\Boscarino\scripts\fib.sh ' . $_GET['integer']);
+  header('Content-Type: application/json');
+	echo json_encode($result);
+?>
