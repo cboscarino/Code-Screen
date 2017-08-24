455e6a42c7b51de71ccd9577d83f98b70a6001a2
cboscarino <31283902+cboscarino@users.noreply.github.com>
2017-08-23 20:50:04 -0400

    Update fibonacci.php
    Formatting

--- fibonacci.php
+++ fibonacci.php
@@ -1,5 +1,5 @@
 <?PHP
 	$result = shell_exec('C:\cygwin64\bin\bash C:\cygwin64\home\Boscarino\scripts\fib.sh ' . $_GET['integer']);
-  header('Content-Type: application/json');
+	header('Content-Type: application/json');
 	echo json_encode($result);
 ?>
