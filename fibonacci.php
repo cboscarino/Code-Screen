5393c2b623c56f3a99801acc2e9699a333354068
cboscarino <31283902+cboscarino@users.noreply.github.com>
2017-08-25 13:07:14 -0400

    Update fibonacci.php
    replacing the call to fib.sh to math.sh with fib argument.

--- fibonacci.php
+++ fibonacci.php
@@ -1,5 +1,5 @@
 <?PHP
-	$result = shell_exec('C:\cygwin64\bin\bash C:\cygwin64\home\Boscarino\scripts\fib.sh ' . $_GET['integer']);
+	$result = shell_exec('C:\cygwin64\bin\bash C:\cygwin64\home\Boscarino\scripts\math.sh fib ' . $_GET['integer']);
 	header('Content-Type: application/json');
 	echo json_encode($result);
 ?>
